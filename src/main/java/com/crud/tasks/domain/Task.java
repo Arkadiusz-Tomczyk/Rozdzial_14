78da5c18173007b8537215eb157c01d1639091f3
“B3zcz31nosc <tomczyk.a@outlook.com>
2018-01-16 21:46:45 +0100

    first commit

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,10 @@
+package com.crud.tasks.domain;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+@Getter
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
