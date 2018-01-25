fb48b0805a4b9fc0a82013a1835d1f6a55639dd4
“B3zcz31nosc <tomczyk.a@outlook.com>
2018-01-25 19:55:56 +0100

    czw

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,10 +1,19 @@
 package com.crud.tasks.domain;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@AllArgsConstructor
 @Getter
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue
     private Long id;
     private String title;
     private String content;
