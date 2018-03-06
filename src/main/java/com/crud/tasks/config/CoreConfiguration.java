a31632cf9013776cefe34146847f35a8496711fa
“G01d3NDUck <tomczyk.a@outlook.com>
2018-03-06 22:23:34 +0100

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,9 +1,7 @@
 package com.crud.tasks.config;
 
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Documentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
