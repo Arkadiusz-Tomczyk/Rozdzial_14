52f42140e26fc1218176d0467c18bd0c500433d1
“G01d3NDUck <tomczyk.a@outlook.com>
2018-04-04 08:31:55 +0200

    heroku

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,19 +1,19 @@
-package com.crud.tasks.controller;
-
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.service.TrelloService;
-import com.crud.tasks.trello.client.TrelloClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-@CrossOrigin("*")
-@RestController
-@RequestMapping("/v1/trello")
-public class TrelloController {
-
-    @Autowired
-    private TrelloService trelloService;
+//package com.crud.tasks.controller;
+//
+//import com.crud.tasks.domain.TrelloBoardDto;
+//import com.crud.tasks.service.TrelloService;
+//import com.crud.tasks.trello.client.TrelloClient;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//@CrossOrigin("*")
+//@RestController
+//@RequestMapping("/v1/trello")
+//public class TrelloController {
+//
+//    @Autowired
+//    private TrelloService trelloService;
 
 //    @RequestMapping(method = RequestMethod.GET, value = "/getTrelloBoards")
 //    public  List<TrelloBoardDto> getTrelloBoards() {
@@ -23,4 +23,4 @@ public class TrelloController {
 //    public CreateTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
 //        return trelloService.createTrelloCard(trelloCardDto);
 //    }
-}
+//}
