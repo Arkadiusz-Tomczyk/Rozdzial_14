6e0a6c909f5de9c6c2ca5aaa8e89ad90b352beec
“G01d3NDUck <tomczyk.a@outlook.com>
2018-04-04 08:33:33 +0200

    heroku

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -1,41 +1,41 @@
-package com.crud.tasks.service;
-
-import com.crud.tasks.config.AdminConfig;
-import com.crud.tasks.domain.Mail;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.trello.client.TrelloClient;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import static java.util.Optional.ofNullable;
-
-@Service
-public class TrelloService {
-    @Autowired
-    private TrelloClient trelloClient;
-
-    @Autowired
-    private SimpleEmailService emailService;
-
-    @Autowired
-    private AdminConfig adminConfig;
-
-    private static final String SUBJECT = "tasks: New trello card";
-
-//    public List<TrelloBoardDto> fetchTrelloBoards() {
-//        return trelloClient.getTrelloBoards();
-//    }
-//    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
-//        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
-//       ofNullable(NewCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(),
-//                "",
-//                SUBJECT,
-//                "new card" + trelloCardDto.getName + "has been created on your trello account"
-//        )));
-//        return newCard;
-//    }
-}
+//package com.crud.tasks.service;
+//
+//import com.crud.tasks.config.AdminConfig;
+//import com.crud.tasks.domain.Mail;
+//import com.crud.tasks.domain.TrelloBoardDto;
+//import com.crud.tasks.trello.client.TrelloClient;
+//import lombok.AllArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//import static java.util.Optional.ofNullable;
+//
+//@Service
+//public class TrelloService {
+//    @Autowired
+//    private TrelloClient trelloClient;
+//
+//    @Autowired
+//    private SimpleEmailService emailService;
+//
+//    @Autowired
+//    private AdminConfig adminConfig;
+//
+//    private static final String SUBJECT = "tasks: New trello card";
+//
+////    public List<TrelloBoardDto> fetchTrelloBoards() {
+////        return trelloClient.getTrelloBoards();
+////    }
+////    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
+////        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+////       ofNullable(NewCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(),
+////                "",
+////                SUBJECT,
+////                "new card" + trelloCardDto.getName + "has been created on your trello account"
+////        )));
+////        return newCard;
+////    }
+//}
