dce210ca8d29d3104302e6e09edfbd4fa980503a
“B3zcz31nosc <tomczyk.a@outlook.com>
2018-04-23 18:19:37 +0200

    24.4

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -1,110 +1,108 @@
-package com.crud.tasks.trello.client;
-
-
-import com.crud.tasks.domain.dto.TrelloBoardDto;
-import com.crud.tasks.trello.config.TrelloConfig;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-//import com.crud.tasks.mapper.CreatedTrelloCardDto;
-
-@RunWith(MockitoJUnitRunner.class)
-public class TrelloClientTest {
-
-    @InjectMocks
-    private TrelloClient trelloClient;
-
-    @Mock
-    private RestTemplate restTemplate;
-
-    @Mock
-    private TrelloConfig trelloConfig;
-
-    @Before
-    public void init() {
-        //Given
-        when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
-        when(trelloConfig.getTrelloAppKey()).thenReturn("test");
-        when(trelloConfig.getTrelloToken()).thenReturn("test");
-        when(trelloConfig.getTrelloUsername()).thenReturn("arkadiusztomczyk1");
-    }
-
-    @Test
-    public void shouldFetchTrelloBoards() throws URISyntaxException {
-
-        TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
-        trelloBoards[0] = new TrelloBoardDto("test_board", "test_id", new ArrayList<>());
-
-        URI uri = new URI("http://test.com/members/arkadiusztomczyk1/boards?key=test&token=test&fields=name,id&lists=all");
-        System.out.println(uri);
-
-        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
-
-        //When
-        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
-
-        //Then
-        assertEquals(1, fetchedTrelloBoards.size());
-        assertEquals("test_id", fetchedTrelloBoards.get(0).getId());
-        assertEquals("test_board", fetchedTrelloBoards.get(0).getName());
-        assertEquals(new ArrayList<>(), fetchedTrelloBoards.get(0).getLists());
-    }
-
-//    @Test
-//    public void shouldCreateCard() throws URISyntaxException {
+//package com.crud.tasks.trello.client;
+//
+//
+//import com.crud.tasks.domain.dto.TrelloBoardDto;
+//import com.crud.tasks.trello.config.TrelloConfig;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.runners.MockitoJUnitRunner;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import java.util.List;
+//
+//import static org.mockito.Mockito.when;
+//
+////import com.crud.tasks.mapper.CreatedTrelloCardDto;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class TrelloClientTest {
+//
+//    @InjectMocks
+//    private TrelloClient trelloClient;
+//
+//    @Mock
+//    private RestTemplate restTemplate;
+//
+//    @Mock
+//    private TrelloConfig trelloConfig;
+//
+//    @Before
+//    public void init() {
 //        //Given
-//        TrelloCardDto trelloCardDto = new TrelloCardDto(
-//                "Test task",
-//                "Test description",
-//                "top",
-//                "test_id"
-//        );
+//        when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+//        when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+//        when(trelloConfig.getTrelloToken()).thenReturn("test");
+//        when(trelloConfig.getTrelloUsername()).thenReturn("arkadiusztomczyk1");
+//    }
 //
-//        URI uri = new URI("http://test.com/cards?key=test&token=test&name=Test%20task&desc=Test%20description&pos=top&idList=test_id");
+////    @Test
+////    public void shouldFetchTrelloBoards() throws URISyntaxException {
+////
+////        TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
+////        trelloBoards[0] = new TrelloBoardDto("test_board", "test_id", new ArrayList<>());
+////
+////        URI uri = new URI("http://test.com/members/arkadiusztomczyk1/boards?key=test&token=test&fields=name,id&lists=all");
+////        System.out.println(uri);
+////
+////        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
+////
+////        //When
+////        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
+////
+////        //Then
+////        assertEquals(1, fetchedTrelloBoards.size());
+////        assertEquals("test_id", fetchedTrelloBoards.get(0).getId());
+////        assertEquals("test_board", fetchedTrelloBoards.get(0).getName());
+////        assertEquals(new ArrayList<>(), fetchedTrelloBoards.get(0).getLists());
+////    }
 //
-//        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto(
-//                "1",
-//                "Test task",
-//                "http://test.com"
-//        );
+////    @Test
+////    public void shouldCreateCard() throws URISyntaxException {
+////        //Given
+////        TrelloCardDto trelloCardDto = new TrelloCardDto(
+////                "Test task",
+////                "Test description",
+////                "top",
+////                "test_id"
+////        );
+////
+////        URI uri = new URI("http://test.com/cards?key=test&token=test&name=Test%20task&desc=Test%20description&pos=top&idList=test_id");
+////
+////        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto(
+////                "1",
+////                "Test task",
+////                "http://test.com"
+////        );
+////
+////        when(restTemplate.postForObject(uri, null, CreatedTrelloCardDto.class)).thenReturn(createdTrelloCardDto);
+////
+////        //When
+////        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
+////        System.out.println(uri);
+////
+////        //Then
+////        assertEquals("1", newCard.getId());
+////        assertEquals("Test task", newCard.getName());
+////        assertEquals("http://test.com", newCard.getShortUrl());
+////    }
+//    @Test
+//    public void shouldReturnEmptyList() throws URISyntaxException {
+//        //Given
+//        URI url = new URI("http://test.com/members/arkadiusztomczyk1/boards?key=test&token=test&fields=name,id&lists=all");
 //
-//        when(restTemplate.postForObject(uri, null, CreatedTrelloCardDto.class)).thenReturn(createdTrelloCardDto);
+//        when(restTemplate.getForObject(url, TrelloBoardDto[].class)).thenReturn(null);
 //
 //        //When
-//        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
-//        System.out.println(uri);
+//        List<TrelloBoardDto> emptyTrelloBoards = trelloClient.getTrelloBoards();
 //
 //        //Then
-//        assertEquals("1", newCard.getId());
-//        assertEquals("Test task", newCard.getName());
-//        assertEquals("http://test.com", newCard.getShortUrl());
+//        Assert.assertEquals(0, emptyTrelloBoards.size());
 //    }
-    @Test
-    public void shouldReturnEmptyList() throws URISyntaxException {
-        //Given
-        URI url = new URI("http://test.com/members/arkadiusztomczyk1/boards?key=test&token=test&fields=name,id&lists=all");
-
-        when(restTemplate.getForObject(url, TrelloBoardDto[].class)).thenReturn(null);
-
-        //When
-        List<TrelloBoardDto> emptyTrelloBoards = trelloClient.getTrelloBoards();
-
-        //Then
-        Assert.assertEquals(0, emptyTrelloBoards.size());
-    }
-}
-
+//}
+//
--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
@@ -1,130 +1,130 @@
-package com.crud.tasks.trello.facade;
-
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloCard;
-import com.crud.tasks.domain.TrelloList;
-import com.crud.tasks.domain.dto.CreatedTrelloCardDto;
-import com.crud.tasks.domain.dto.TrelloBoardDto;
-import com.crud.tasks.domain.dto.TrelloCardDto;
-import com.crud.tasks.domain.dto.TrelloListDto;
-import com.crud.tasks.mapper.TrelloMapper;
-import com.crud.tasks.service.TrelloService;
-import com.crud.tasks.trello.validator.TrelloValidator;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.anyList;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
-public class TrelloFacadeTest {
-    @InjectMocks
-    private TrelloFacade trelloFacade;
-    @Mock
-    private TrelloValidator trelloValidator;
-    @Mock
-    private TrelloService trelloService;
-    @Mock
-    private TrelloMapper trelloMapper;
-
-
-    @Test
-    public void shouldFetchEmptyList() {
-        //Given
-        List<TrelloList> mappedTrelloList = new ArrayList<>();
-        mappedTrelloList.add(new TrelloList("1", "test_list", false));
-
-        List<TrelloListDto> trelloList = new ArrayList<>();
-        trelloList.add(new TrelloListDto("1", "test_list", false));
-
-        List<TrelloBoard> mappedTrelloBoards = new ArrayList<>();
-        mappedTrelloBoards.add(new TrelloBoard("1", "test", mappedTrelloList));
-
-        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
-        trelloBoards.add(new TrelloBoardDto("1", "test", trelloList));
-
-        when(trelloService.fetchTrelloBoards()).thenReturn(trelloBoards);
-        when(trelloMapper.mapToBoard(trelloBoards)).thenReturn(mappedTrelloBoards);
-        when(trelloMapper.mapToBoardDto(anyList())).thenReturn(new ArrayList<>());
-        when(trelloValidator.validateBoard(mappedTrelloBoards)).thenReturn(new ArrayList<>());
-
-        //When
-        List<TrelloBoardDto> trelloBoardDtos = trelloFacade.fetchTrelloBoards();
-
-        //Then
-        assertNotNull(trelloBoardDtos);
-        assertEquals(0, trelloBoardDtos.size());
-    }
-
-    @Test
-    public void shouldFetchTrelloBoards() {
-        //Given
-        List<TrelloList> mappedTrelloList = new ArrayList<>();
-        mappedTrelloList.add(new TrelloList("1", "my_list", false));
-
-        List<TrelloListDto> trelloList = new ArrayList<>();
-        trelloList.add(new TrelloListDto("1", "my_list", false));
-
-        List<TrelloBoard> mappedTrelloBoards = new ArrayList<>();
-        mappedTrelloBoards.add(new TrelloBoard("1", "my_task", mappedTrelloList));
-
-        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
-        trelloBoards.add(new TrelloBoardDto("1", "my_task", trelloList));
-
-        when(trelloService.fetchTrelloBoards()).thenReturn(trelloBoards);
-        when(trelloMapper.mapToBoard(trelloBoards)).thenReturn(mappedTrelloBoards);
-        when(trelloMapper.mapToBoardDto(anyList())).thenReturn(trelloBoards);
-        when(trelloValidator.validateBoard(mappedTrelloBoards)).thenReturn(mappedTrelloBoards);
-
-        //When
-        List<TrelloBoardDto> trelloBoardDtos = trelloFacade.fetchTrelloBoards();
-
-        //Then
-        //Then
-        assertNotNull(trelloBoardDtos);
-        assertEquals(1, trelloBoardDtos.size());
-
-        trelloBoardDtos.forEach(trelloBoardDto -> {
-            assertEquals("1", trelloBoardDto.getId());
-            assertEquals("my_task", trelloBoardDto.getName());
-
-            trelloBoardDto.getLists().forEach(trelloListDto -> {
-                assertEquals("1", trelloListDto.getId());
-                assertEquals("my_list", trelloListDto.getName());
-                assertEquals(false,trelloListDto.isClosed());
-            });
-        });
-
-    }
-
-    @Test
-    public void testCreateNewCard() throws MalformedURLException {
-        //given
-        TrelloCardDto trelloCardDto = new TrelloCardDto("test", "test1", "top", "1");
-        TrelloCard trelloCard = new TrelloCard("test", "test1", "top", "1");
-        CreatedTrelloCardDto createTrelloCardDto = new CreatedTrelloCardDto("1", "test", "any");
-
-        when(trelloMapper.mapToCard(trelloCardDto)).thenReturn(trelloCard);
-        doNothing().when(trelloValidator).validateCard(trelloCard);
-        when(trelloMapper.mapToCardDto(trelloCard)).thenReturn(trelloCardDto);
-        when(trelloService.createTrelloCard(trelloCardDto)).thenReturn(createTrelloCardDto);
-        //when
-        CreatedTrelloCardDto card = trelloFacade.createCard(trelloCardDto);
-
-        //then
-        assertEquals("test",card.getName());
-        assertEquals("1", card.getId());
-        assertEquals("any", card.getShortUrl());
-
-    }
-}
+//package com.crud.tasks.trello.facade;
+//
+//import com.crud.tasks.domain.TrelloBoard;
+//import com.crud.tasks.domain.TrelloCard;
+//import com.crud.tasks.domain.TrelloList;
+//import com.crud.tasks.domain.dto.CreatedTrelloCardDto;
+//import com.crud.tasks.domain.dto.TrelloBoardDto;
+//import com.crud.tasks.domain.dto.TrelloCardDto;
+//import com.crud.tasks.domain.dto.TrelloListDto;
+//import com.crud.tasks.mapper.TrelloMapper;
+//import com.crud.tasks.service.TrelloService;
+//import com.crud.tasks.trello.validator.TrelloValidator;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.runners.MockitoJUnitRunner;
+//
+//import java.net.MalformedURLException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.mockito.Matchers.anyList;
+//import static org.mockito.Mockito.doNothing;
+//import static org.mockito.Mockito.when;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class TrelloFacadeTest {
+//    @InjectMocks
+//    private TrelloFacade trelloFacade;
+//    @Mock
+//    private TrelloValidator trelloValidator;
+//    @Mock
+//    private TrelloService trelloService;
+//    @Mock
+//    private TrelloMapper trelloMapper;
+//
+//
+//    @Test
+//    public void shouldFetchEmptyList() {
+//        //Given
+//        List<TrelloList> mappedTrelloList = new ArrayList<>();
+//        mappedTrelloList.add(new TrelloList("1", "test_list", false));
+//
+//        List<TrelloListDto> trelloList = new ArrayList<>();
+//        trelloList.add(new TrelloListDto("1", "test_list", false));
+//
+//        List<TrelloBoard> mappedTrelloBoards = new ArrayList<>();
+//        mappedTrelloBoards.add(new TrelloBoard("1", "test", mappedTrelloList));
+//
+//        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
+//        trelloBoards.add(new TrelloBoardDto("1", "test", trelloList));
+//
+//        when(trelloService.fetchTrelloBoards()).thenReturn(trelloBoards);
+//        when(trelloMapper.mapToBoard(trelloBoards)).thenReturn(mappedTrelloBoards);
+//        when(trelloMapper.mapToBoardDto(anyList())).thenReturn(new ArrayList<>());
+//        when(trelloValidator.validateBoard(mappedTrelloBoards)).thenReturn(new ArrayList<>());
+//
+//        //When
+//        List<TrelloBoardDto> trelloBoardDtos = trelloFacade.fetchTrelloBoards();
+//
+//        //Then
+//        assertNotNull(trelloBoardDtos);
+//        assertEquals(0, trelloBoardDtos.size());
+//    }
+//
+//    @Test
+//    public void shouldFetchTrelloBoards() {
+//        //Given
+//        List<TrelloList> mappedTrelloList = new ArrayList<>();
+//        mappedTrelloList.add(new TrelloList("1", "my_list", false));
+//
+//        List<TrelloListDto> trelloList = new ArrayList<>();
+//        trelloList.add(new TrelloListDto("1", "my_list", false));
+//
+//        List<TrelloBoard> mappedTrelloBoards = new ArrayList<>();
+//        mappedTrelloBoards.add(new TrelloBoard("1", "my_task", mappedTrelloList));
+//
+//        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
+//        trelloBoards.add(new TrelloBoardDto("1", "my_task", trelloList));
+//
+//        when(trelloService.fetchTrelloBoards()).thenReturn(trelloBoards);
+//        when(trelloMapper.mapToBoard(trelloBoards)).thenReturn(mappedTrelloBoards);
+//        when(trelloMapper.mapToBoardDto(anyList())).thenReturn(trelloBoards);
+//        when(trelloValidator.validateBoard(mappedTrelloBoards)).thenReturn(mappedTrelloBoards);
+//
+//        //When
+//        List<TrelloBoardDto> trelloBoardDtos = trelloFacade.fetchTrelloBoards();
+//
+//        //Then
+//        //Then
+//        assertNotNull(trelloBoardDtos);
+//        assertEquals(1, trelloBoardDtos.size());
+//
+//        trelloBoardDtos.forEach(trelloBoardDto -> {
+//            assertEquals("1", trelloBoardDto.getId());
+//            assertEquals("my_task", trelloBoardDto.getName());
+//
+//            trelloBoardDto.getLists().forEach(trelloListDto -> {
+//                assertEquals("1", trelloListDto.getId());
+//                assertEquals("my_list", trelloListDto.getName());
+//                assertEquals(false,trelloListDto.isClosed());
+//            });
+//        });
+//
+//    }
+//
+//    @Test
+//    public void testCreateNewCard() throws MalformedURLException {
+//        //given
+//        TrelloCardDto trelloCardDto = new TrelloCardDto("test", "test1", "top", "1");
+//        TrelloCard trelloCard = new TrelloCard("test", "test1", "top", "1");
+//        CreatedTrelloCardDto createTrelloCardDto = new CreatedTrelloCardDto("1", "test", "any");
+//
+//        when(trelloMapper.mapToCard(trelloCardDto)).thenReturn(trelloCard);
+//        doNothing().when(trelloValidator).validateCard(trelloCard);
+//        when(trelloMapper.mapToCardDto(trelloCard)).thenReturn(trelloCardDto);
+//        when(trelloService.createTrelloCard(trelloCardDto)).thenReturn(createTrelloCardDto);
+//        //when
+//        CreatedTrelloCardDto card = trelloFacade.createCard(trelloCardDto);
+//
+//        //then
+//        assertEquals("test",card.getName());
+//        assertEquals("1", card.getId());
+//        assertEquals("any", card.getShortUrl());
+//
+//    }
+//}
